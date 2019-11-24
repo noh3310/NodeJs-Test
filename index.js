e64bec396767e32224ba8b749ccdcb33da7f6570
noh3310 <noh3310@gmail.com>
2019-11-24 18:23:56 +0900

    init heroku

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-var http = require('http');
-var fs = require('fs');
-var express = require('express');
-var bodyParser = require('body-parser');
-var ejs = require('ejs');
- 
-var app = express();
-app.use(bodyParser());
-var router = express.Router();
-app.use(express.static('public'));
-app.use(router);
+var app = require('express')();
+var http = require('http').Server(app);
+//var io = require('socket.io')(http);
 
-var port = process.env.PORT || 3000;
+var MongoClient = require('mongodb').MongoClient;
+var database;
+function connectDB() {
+  var url = "mongodb://<dbuser>:<dbpassword>@ds029787.mlab.com:29787/heroku_1d5zm8cr";
 
-router.get("/",function(req,res){
- res.send("<h1>hello heroku node.js world</h1>");
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log('데이터베이스에 연결되었습니다. : ' + url);
+    // database 변수에 할당
+    database = db;
+  });
+}
+
+// 로그인 요청
+app.get('/Login', function (req, res) {
+  var id = req.param("id");
+  var pw = req.param('pw');
+  var type = req.param('type');
+
+  res.writeHead("200", {"Content-Type":"text/html;charset=utf8"});
+	res.write("user id : " + id);
+	res.write("<br>");
+  res.write("userd pw : " + pw);
+  res.write("<br>");
+  res.write("userd type : " + type);
+	res.end();
 });
 
-http.createServer(app).listen(port, function(){
- console.log('server run');
+
+http.listen(9000, function () {
+  connectDB();
+  console.log('server listening on port 9000');
 });
